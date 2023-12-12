c6030dd8bd0e43a1d599d79f850347e2ada3857e
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2023-12-12 22:10:36 +0500

    All done

---
+++ index.js
@@ -0,0 +1,185 @@
+    // 1-masala
+// let a = +prompt("Birinchi sonni kiriting...");
+// let b = +prompt("Ikkinchi sonni kiriting...");
+// let c = +prompt("Uchinchi sonni kiriting...");
+
+// let total = 1;
+
+// if (a % 2 == 0 && a % 5 == 0) {
+//     total *= a;
+// }
+
+// if (b % 2 == 0 && a % 5 == 0) {
+//     total *= b;
+// }
+
+// if (c % 2 == 0 && a % 5 == 0) {
+//     total *= c;
+// }
+
+// console.log(total);
+
+
+
+
+    // 2-masala
+// let a = +prompt("Birinchi sonni kiriting...");
+// let b = +prompt("Ikkinchi sonni kiriting...");
+// let c = +prompt("Uchinchi sonni kiriting...");
+// let d = +prompt("Tortinchi sonni kiriting...");
+
+// let total = 1;
+
+// if (a % 3 == 0 || a % 7 == 0) {
+//     total *= a;
+// }
+
+// if (b % 3 == 0 || b % 7 == 0) {
+//     total *= b;
+// }
+
+// if (c % 7 == 0 || a % 3 == 0) {
+//     total *= c;
+// }
+
+// if (d % 3 == 0 || a % 7 == 0) {
+//     total *= d;
+// }
+
+// console.log(total);
+
+
+
+    // 3-masala
+// let a = +prompt("Sonni kiriting...");
+// let one = a % 10;
+// let ten = Math.trunc(a / 10);
+// let res = one * ten
+
+// console.log(res);
+
+
+
+    // 4-masala
+// let a = +prompt(`Sonni kiriting...`);
+// let ten = Math.trunc(a / 10);
+
+// if (ten % 2 == 0) {
+//     a = a ** 2;
+// } else {
+//     a = a ** 3;
+// }
+
+// console.log(a);
+
+
+
+    // 5-masala 
+// let a = +prompt(`Sonni kiriting...`);
+// console.log(Math.trunc((a / 10) % 10));
+// console.log(Math.trunc(a / 100));
+
+
+
+    // 6-masala 
+// let a = +prompt('Sonni kiriting...');
+// let one = a % 10;
+// let ten = (Math.trunc(a / 10)) % 10;
+// let hundred = Math.trunc(a / 100);
+// let res = 1;
+
+// if (one % 2 == 1) {
+//     res = res * one;
+// }
+
+// if (ten % 2 == 1) {
+//     res = res * ten;
+// }
+
+// if (hundred % 2 == 1) {
+//     res = res * hundred;
+// }
+
+// console.log(res);
+
+
+
+    // 7-masala
+// let a = +prompt(`Sonni kiriting...`);
+// let one = a % 10;
+// let ten = (Math.trunc(a / 10)) % 10;
+// let hundred = Math.trunc(a / 100);
+// let thousand = Math.trunc(a / 1000);
+
+// let counter = 0;
+
+// if (one % 2 == 0) {
+//     counter++;
+// }
+
+// if (ten % 2 == 0) {
+//     counter++;
+// }
+
+// if (hundred % 2 == 0) {
+//     counter++;
+// }
+
+// if (thousand % 2 == 0) {
+//     counter++;
+// }
+
+// console.log(counter);
+
+
+
+    // 8-masala 
+// let a = +prompt('Tomonini kiriting...');
+// let b = +prompt('Tomonini kiriting...');
+// let P = 2 * (a + b);
+// let S = a * b;
+
+// if (P > S) {
+//     console.log(P);
+// } else {
+//     console.log(S);
+// }
+
+
+
+    // 9-masala 
+// let a = +prompt(`Sonni kiriting...`);
+// let one = a % 10;
+// let ten = (Math.trunc(a / 10)) % 10;
+// let hundred = Math.trunc(a / 100);
+// let thousand = Math.trunc(a / 1000);
+// let five = Math.trunc(a / 10000);
+
+// let counter = 0;
+
+// if (one == 0) {
+//     counter++;
+// }
+
+// if (ten == 0) {
+//     counter++;
+// }
+
+// if (hundred == 0) {
+//     counter++;
+// }
+
+// if (thousand == 0) {
+//     counter++;
+// }
+
+// if (five == 0) {
+//     counter++;
+// }
+
+// console.log(counter);
+
+
+
+    // 10
+// chunmadm(((
